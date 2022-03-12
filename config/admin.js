447d0ba9338e7dfe129a2caa26070d44f1e84316
gimy <b.afatah@yahoo.com>
2022-03-12 12:31:06 +0800

    fix: use default value for url

--- config/admin.js
+++ config/admin.js
@@ -2,5 +2,5 @@ module.exports = ({ env }) => ({
   auth: {
     secret: env("ADMIN_JWT_SECRET", "aaa6687f774e33718906682dc39996ee"),
   },
-  url: env("PUBLIC_ADMIN_URL", "/admin"),
+  url: env("PUBLIC_ADMIN_URL", "/admin-content"),
 });
