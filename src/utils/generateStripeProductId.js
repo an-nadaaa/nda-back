2882c454d9fb2f52657ea962ea03054038efbfe4
gimy <b.afatah@yahoo.com>
2022-03-13 10:25:55 +0800

    fix: remove mutability problem in the product generation function

--- src/utils/generateStripeProductId.js
+++ src/utils/generateStripeProductId.js
@@ -6,10 +6,9 @@ const FUNCTIONS_BASE_URL =
     ? process.env.FUNCTIONS_BASE_URL
     : "http://localhost:8888/.netlify/functions";
 // this function is used to generate a stripe product and return it's id
-module.exports = async (entity, url) => {
-  if (url) {
-    entity.cover = url;
-  }
+module.exports = async (eventData, url) => {
+  // we use object assign to not mutate the eventData to avoid side effects see more: https://trimagency.com/blogs/immutable-objects-using-object-assign/#:~:text=Now%20that's%20immutable!,our%20'source'%20objects%20intact.
+  const entity = Object.assign({}, eventData, { cover: url });
   const { data } = await axios({
     url: `${FUNCTIONS_BASE_URL}/products-handler`,
     method: "POST",
