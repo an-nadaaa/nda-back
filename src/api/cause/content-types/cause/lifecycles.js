739adbe1b64f80a644298bac4c8dd5bb6d6e3722
gimy <b.afatah@yahoo.com>
2022-03-27 19:59:06 +0800

    feat: add lifecycle function for product creation

---
+++ src/api/cause/content-types/cause/lifecycles.js
@@ -0,0 +1,91 @@
+const generateStripeProductId = require("../../../../utils/generateStripeProductId");
+require("dotenv").config();
+const { default: axios } = require("axios");
+
+const STRIPE_GENERAL_PRODUCT_ID_DEV = process.env.STRIPE_GENERAL_PRODUCT_ID_DEV;
+const STRIPE_GENERAL_PRODUCT_ID_PROD =
+  process.env.STRIPE_GENERAL_PRODUCT_ID_PROD;
+const FUNCTION_BASE_URL =
+  process.env.NODE_ENV === "production"
+    ? process.env.FUNCTIONS_BASE_URL
+    : "http://localhost:8888/.netlify/functions";
+
+module.exports = {
+  async beforeCreate(event) {
+    const { data } = event.params;
+    const { title, description } = await strapi.db
+      .query("causes.base-model")
+      .findOne({ id: data.base.id });
+    if (data.cover) {
+      const { url } = await strapi.plugins.upload.services.upload.findOne(
+        data.cover
+      );
+      event.params.data.product = await generateStripeProductId(
+        title,
+        description,
+        url,
+        data.product,
+        data.environment
+      );
+    } else {
+      // generate a stripe product and replace the default product property with the stripe product id
+      event.params.data.product = await generateStripeProductId(
+        title,
+        description,
+        undefined,
+        data.product,
+        data.environment
+      );
+    }
+  },
+
+  async beforeUpdate(event) {
+    const { data } = event.params;
+    // generate a stripe product and replace the default product property with the stripe product id
+    if (
+      data.product === STRIPE_GENERAL_PRODUCT_ID_DEV ||
+      data.product === STRIPE_GENERAL_PRODUCT_ID_PROD ||
+      data.product === "PRODUCT_WILL_BE_CREATED"
+    ) {
+      let id;
+      const { title, description } = await strapi.db
+        .query("causes.base-model")
+        .findOne({ id: data.base.id });
+      if (data.cover) {
+        const { url } = await strapi.plugins.upload.services.upload.findOne(
+          data.cover
+        );
+        id = await generateStripeProductId(
+          title,
+          description,
+          url,
+          data.product,
+          data.environment
+        );
+      } else {
+        id = await generateStripeProductId(
+          title,
+          description,
+          undefined,
+          data.product,
+          data.environment
+        );
+      }
+      event.params.data.product = id;
+    }
+  },
+
+  async afterDelete(event) {
+    // delete the stripe product for development environment
+    if (event.result.product !== STRIPE_GENERAL_PRODUCT_ID_DEV) {
+      await axios({
+        url: `${FUNCTION_BASE_URL}/products-handler`,
+        method: "DELETE",
+        headers: {
+          "Content-Type": "application/json",
+        },
+        data: JSON.stringify(event.result),
+      });
+    }
+  },
+};
