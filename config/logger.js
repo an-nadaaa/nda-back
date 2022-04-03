6afea4dba096b0cc471b5accce5bc918d412cb59
gimy <b.afatah@yahoo.com>
2022-04-03 16:36:07 +0800

    feat: use ecs log formates for production

---
+++ config/logger.js
@@ -0,0 +1,34 @@
+"use strict";
+
+const {
+  winston,
+  formats: { prettyPrint, levelFilter },
+} = require("@strapi/logger");
+const ecsFormat = require("@elastic/ecs-winston-format");
+// import stripAnsi from "strip-ansi";
+
+const removeAnsi = (module.exports =
+  process.env.NODE_ENV === "production"
+    ? {
+        transports: [
+          new winston.transports.Console({
+            level: "http",
+            format: winston.format.combine(
+              levelFilter("http"),
+              winston.format.uncolorize(),
+              ecsFormat()
+            ),
+          }),
+        ],
+      }
+    : {
+        transports: [
+          new winston.transports.Console({
+            level: "http",
+            format: winston.format.combine(
+              levelFilter("http"),
+              prettyPrint({ timestamps: "YYYY-MM-DD hh:mm:ss.SSS" })
+            ),
+          }),
+        ],
+      });
